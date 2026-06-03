ffd051350a66f15de703a9b79bf03a1a1c7cadde
Brynner Jay Antiquesa <imbamystery@gmail.com>
2026-06-03 17:49:29 +0800

    fix generation of grids inside of the container and change it into dynamic generation of grids

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-const gridItems = document.querySelectorAll('.grid-item');
 const resizeButton = document.querySelector('#resize-button');
 const container = document.querySelector('.container');
 
 let gridNum = 16;
 
-gridItems.forEach(item => {
-    item.addEventListener('mouseenter', () => {
-        item.classList.add('active');
+function generateGrid(num) {
+    for (let i = 0; i < (num * num); i++) {
+        const item = document.createElement('div');
+        item.classList.add('grid-item');
+        container.appendChild(item);
+    }
+    container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+    const gridItems = document.querySelectorAll('.grid-item');
+    gridItems.forEach(item => {
+        item.addEventListener('mouseenter', () => {
+            item.classList.add('active');
+        });
     });
-});
+}
 
 resizeButton.addEventListener('click', (event) => {
     gridNum = prompt("Input grids in width and height:");
+    container.replaceChildren();
     container.style.gridTemplateColumns = `repeat(${gridNum}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${gridNum}, 1fr)`;
+    generateGrid(gridNum);
 });
+
+generateGrid(gridNum);
