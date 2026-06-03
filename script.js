bc3d39d927ee2f1cb76c3ae291459eefb09d88bf
Brynner Jay Antiquesa <imbamystery@gmail.com>
2026-06-03 17:04:02 +0800

    added functionality to resize-button to control how many grids

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const gridItems = document.querySelectorAll('.grid-item');
+const resizeButton = document.querySelector('#resize-button');
+const container = document.querySelector('.container');
+
+let gridNum = 16;
 
 gridItems.forEach(item => {
     item.addEventListener('mouseenter', () => {
         item.classList.add('active');
     });
+});
+
+resizeButton.addEventListener('click', (event) => {
+    gridNum = prompt("Input grids in width and height:");
+    container.style.gridTemplateColumns = `repeat(${gridNum}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${gridNum}, 1fr)`;
 });
